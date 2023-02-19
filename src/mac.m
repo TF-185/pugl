5a252028005b54a69e490757b486cd5a09158f51
David Robillard <d@drobilla.net>
2023-02-19 15:29:37 -0500

    Make puglSetSizeHint() take normal width unsigned integers
    This is more convenient for user code since you can do arithmetic and not have
    to cast back down to PuglSpan.  The implementation will dynamically handle out
    of range parameters.

--- src/mac.m
+++ src/mac.m
@@ -1810,15 +1810,16 @@ puglSetSize(PuglView* const view, const unsigned width, const unsigned height)
 PuglStatus
 puglSetSizeHint(PuglView* const    view,
                 const PuglSizeHint hint,
-                const PuglSpan     width,
-                const PuglSpan     height)
+                const unsigned     width,
+                const unsigned     height)
 {
-  if ((unsigned)hint >= PUGL_NUM_SIZE_HINTS) {
+  if ((unsigned)hint >= PUGL_NUM_SIZE_HINTS || width > INT16_MAX ||
+      height > INT16_MAX) {
     return PUGL_BAD_PARAMETER;
   }
 
-  view->sizeHints[hint].width  = width;
-  view->sizeHints[hint].height = height;
+  view->sizeHints[hint].width  = (PuglSpan)width;
+  view->sizeHints[hint].height = (PuglSpan)height;
 
   return (hint == PUGL_CURRENT_SIZE) ? puglSetSize(view, hint, width, height)
                                      : PUGL_SUCCESS;
